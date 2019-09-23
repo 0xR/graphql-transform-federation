4a3699975fc44baeaa8a11ce92c94ded02243be6
Ruben Oostinga <ruben.oostinga@gmail.com>
2019-09-23 15:22:26 +0200

    Make addKeyDirective configurable

--- src/transform-sdl.spec.ts
+++ src/transform-sdl.spec.ts
@@ -1,16 +1,45 @@
 import dedent from 'dedent';
-import { addKeyDirective } from "./transform-sdl";
+import { addKeyDirective } from './transform-sdl';
 
 describe('transform-federation', () => {
   it('should add key directives to sdl', () => {
     expect(
-      addKeyDirective(dedent`
-      type Product {
+      addKeyDirective(
+        `
+      type Product @keep {
         id: Int
-      }`),
+      }`,
+        {
+          Product: {
+            keyFields: ['id'],
+          },
+        },
+      ),
     ).toEqual(dedent`
-      type Product @key(fields: "id") {
+      type Product @keep @key(fields: "id") {
         id: Int
       }\n`);
+  });
+
+  it('should throw an error if not all keys were added', () => {
+    expect(() => {
+      addKeyDirective(
+        `
+        type Product {
+          id: Int
+        }
+      `,
+        {
+          NotProduct: {
+            keyFields: ['mock keyFields'],
+          },
+          NotProduct2: {
+            keyFields: ['mock keyFields'],
+          },
+        },
+      );
+    }).toThrow(
+      'Could not add key directives to types: NotProduct, NotProduct2',
+    );
   });
 });
--- src/transform-sdl.ts
+++ src/transform-sdl.ts
@@ -1,9 +1,7 @@
 import { DirectiveNode, parse, print, visit } from 'graphql/language';
 
-export function addKeyDirective(schema: string): string {
-  const ast = parse(schema);
-
-  const directive: DirectiveNode = {
+function createKeyDirectiveNode(fields: string): DirectiveNode {
+  return {
     kind: 'Directive',
     name: {
       kind: 'Name',
@@ -18,20 +16,51 @@ export function addKeyDirective(schema: string): string {
         },
         value: {
           kind: 'StringValue',
-          value: 'id',
+          value: fields,
         },
       },
     ],
   };
+}
+
+export interface KeyDirectiveConfig {
+  [typeName: string]: {
+    keyFields: string[];
+  };
+}
+
+export function addKeyDirective(
+  schema: string,
+  config: KeyDirectiveConfig,
+): string {
+  const ast = parse(schema);
+
+  const typesTodo = new Set(Object.keys(config));
+
   const withDirectives = visit(ast, {
     ObjectTypeDefinition: {
       enter(node) {
-        return {
-          ...node,
-          directives: [...(node.directives || []), directive],
-        };
+        const typeName = node.name.value;
+        if (typesTodo.has(typeName)) {
+          typesTodo.delete(typeName);
+
+          const newDirectives = config[typeName].keyFields.map(keyField =>
+            createKeyDirectiveNode(keyField),
+          );
+          return {
+            ...node,
+            directives: [
+              ...(node.directives || []),
+              ...newDirectives
+            ],
+          };
+        }
       },
     },
   });
+
+  if(typesTodo.size !== 0) {
+    throw new Error(`Could not add key directives to types: ${Array.from(typesTodo).join(', ')}`)
+  }
   return print(withDirectives);
 }
